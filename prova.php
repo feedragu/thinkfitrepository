ec3753aa51b870389e949680b6a740c61d16bbbb
feedragu <ragusafederico97@gmail.com>
2017-10-16 16:49:28 +0200

    algoritmo fico

--- prova.php
+++ prova.php
@@ -6,4 +6,16 @@
 			header('Location: .index.html');
         } 
 echo "gravida giovenca";
+
+
+function random_str($length, $keyspace = '0123456789')
+{
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+    }
+    return $str;
+}
+
 ?>  
