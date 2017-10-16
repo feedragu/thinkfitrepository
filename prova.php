cd48260de8775c4d72ef999559fb7d1ff43ea55f
Ekos27 <neekos97>
2017-10-16 13:13:17 +0200

    Commit

--- prova.php
+++ prova.php
@@ -1,8 +1,8 @@
 <?php 
-   $con = new mysqli("89.46.111.27", "", "177p047266" , "Sql1006791_4");
+   $con = new mysqli("89.46.111.27", "Sql1006791", "177p047266" , "Sql1006791_4");
     // Check connection
         if ($con->connect_error) {
             die("Connection failed: " . $con->connect_error);
 			header('Location: .index.html');
         } 
-?>
+?>  
