118eab917a9fd9c42dfdd4daf81ffe9a993f624d
feedragu <ragusafederico97@gmail.com>
2017-10-16 13:09:12 +0200

    cvxzc

--- prova.php
+++ prova.php
@@ -1,5 +1,5 @@
 <?php 
-   $con = new mysqli("89.46.111.27", "root", "" , "Sql1006791_4");
+   $con = new mysqli("89.46.111.27", "", "177p047266" , "Sql1006791_4");
     // Check connection
         if ($con->connect_error) {
             die("Connection failed: " . $con->connect_error);
