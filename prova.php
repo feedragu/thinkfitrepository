0e6a77e4b6efd7b75c8e91ceefdc5ea6b256b831
Ekos27 <neekos97>
2017-10-16 17:18:32 +0200

    Fatto bene la prova col PHP quello bello

--- prova.php
+++ prova.php
@@ -5,6 +5,10 @@
             die();
 			header('Location: .index.html');
         } 
-echo "gravida giovenca";
-
+echo "Benvenuto!";
+mysqli_select_db($con, "thinkfit");
+$result = mysqli_query($con, "SELECT * FROM users WHERE nome = 'Federico'");
+if(mysqli_num_rows($result)>0)
+	while($row=mysqli_fetch_row($result))
+		echo $row[1];
 ?>  
