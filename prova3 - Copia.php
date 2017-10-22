f82e32f1782f436a8bb131234943dc287a357472
feedragu <ragusafederico97@gmail.com>
2017-10-22 13:10:40 +0200

    Creazione file di prova per testare permessi unix che mannaggia a loro guarda

--- prova3 - Copia.php
+++ prova3 - Copia.php
@@ -1,16 +1,14 @@
 <?php 
-  $con =  mysqli_connect("localhost:3306", "root", "test" , "thinkfit"); 
+  $con =  mysqli_connect("localhost", "root", "test" , "thinkfit"); 
     // Check connection
         if (!$con) {
             die();
 			header('Location: .index.html');
         } 
 echo "Benvenuto!";
-mysqli_select_db($con, "thinkfit");
-$result = mysqli_query($con, "SELECT * FROM prova");
-
+mysqli_select_db($con, "prova");
+$result = mysqli_query($con, "SELECT * FROM users WHERE nome = 'Federico'");
 if(mysqli_num_rows($result)>0)
 	while($row=mysqli_fetch_row($result))
-		echo $row[0];
-echo "Benvenuto!";
+		echo $row[1];
 ?>  
