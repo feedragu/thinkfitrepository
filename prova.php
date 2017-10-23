a468a98d6af07965c6f4d01904c2bdbef7330a06
Ekos27 <neekos97>
2017-10-23 17:10:26 +0200

    Ajsajhska

--- prova.php
+++ prova.php
@@ -7,10 +7,10 @@
         } 
 echo "Benvenuto!";
 mysqli_select_db($con, "thinkfit");
-$result = mysqli_query($con, "SELECT * FROM prova");
+$result = mysqli_query($con, "SELECT nome, cognome FROM users");
 
 if(mysqli_num_rows($result)>0)
 	while($row=mysqli_fetch_row($result))
-		echo $row[0];
-echo "Benvenuto!";
+		echo "Benvenuto ".$row[0]." ".$row[1];
+
 ?>  
